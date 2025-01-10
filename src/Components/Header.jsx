fe892fa0b1c3cc5ce5982c47dd43758fde43f868
only <66200423@kmitl.ac.th>
2025-01-10 16:29:15 +0700

    2nd commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header >
+    <header>
         <div className="container flex justify-center items-center mx-auto p-4">
-            <h1 className="text-4xl font-bold text-[#FF5C8D] py-5 hover:opacity-50 transition cursor-pointer ">Friend IG</h1>
+            <h1 className="text-5xl font-bold text-[#FF5C8D] py-5 hover:opacity-50 transition cursor-default md:text-6xl lg:text-7xl">Friend IG</h1>
         </div>
     </header>
   )
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,38 +3,38 @@ import React from 'react';
 const Main = () => {
     return (
         <main>
-            <div className="grid grid-cols-1 px-[6vh] mt-10 mb-16 md:grid-cols-2 lg:grid-cols-3 gap-16">
-                <div className="relative shadow-lg rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 ">
+            <div className="grid grid-cols-1 px-[7vh] mt-14 mb-16 md:grid-cols-2 lg:grid-cols-3 gap-24">
+                <div className="relative rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 hover:opacity-50 transition cursor-pointer">
                     <a href="https://www.instagram.com/userrnamebxnk/" target="_blank">
                         <img src="/src/assets/bank.jpg" alt="random" className="object-cover object-center" />
                         <p className="absolute top-4 left-6 text-[#FF5C8D] text-xl font-semibold">Bank</p>
                     </a>
                 </div>
-                <div className="relative shadow-lg rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 ">
+                <div className="relative rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 hover:opacity-50 transition cursor-pointer">
                     <a href="https://www.instagram.com/amn_n04" target="_blank">
                         <img src="/src/assets/god.jpg" alt="random" className="object-cover object-center" />
                         <p className="absolute top-4 left-6 text-[#FF5C8D] text-xl font-semibold">God</p>
                     </a>
                 </div>
-                <div className="relative shadow-lg rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 ">
+                <div className="relative rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 hover:opacity-50 transition cursor-pointer">
                     <a href="https://www.instagram.com/jxegyy/" target="_blank">
                         <img src="/src/assets/joe.jpg" alt="random" className="object-cover object-center" />
                         <p className="absolute top-4 left-6 text-[#FF5C8D] text-xl font-semibold">Joe</p>
                     </a>
                 </div>
-                <div className="relative shadow-lg rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 ">
+                <div className="relative rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 hover:opacity-50 transition cursor-pointer">
                     <a href="https://www.instagram.com/mansithawut" target="_blank">
                         <img src="/src/assets/man.jpg" alt="random" className="object-cover object-center" />
                         <p className="absolute top-4 left-6 text-[#FF5C8D] text-xl font-semibold">Man</p>
                     </a>
                 </div>
-                <div className="relative shadow-lg rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 ">
+                <div className="relative rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 hover:opacity-50 transition cursor-pointer">
                     <a href="https://www.instagram.com/_47px/" target="_blank">
                         <img src="/src/assets/ploy.jpg" alt="random" className="object-cover object-center" />
                         <p className="absolute top-4 left-6 text-[#FF5C8D] text-xl font-semibold">Pimploy</p>
                     </a>
                 </div>
-                <div className="relative shadow-lg rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 ">
+                <div className="relative rounded-3xl overflow-hidden shadow-lg shadow-[#FF5C8D]/50 hover:opacity-50 transition cursor-pointer">
                     <a href="https://www.instagram.com/sxaxh_luv" target="_blank">
                         <img src="/src/assets/smash.jpg" alt="random" className="object-cover object-center" />
                         <p className="absolute top-4 left-6 text-[#FF5C8D] text-xl font-semibold">Smash</p>
